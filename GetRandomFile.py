d5d676bdf9ff3dcd472b5eaccb733934e3c2f02b
Zhaopeng Zhang <zhangzhaopeng@mail.nankai.edu.cn>
2017-09-09 16:45:01 +0800

    Update Get file with specific number of line

--- GetRandomFile.py
+++ GetRandomFile.py
@@ -13,10 +13,19 @@ def getRandomGFile(filename, G):
             for i in range(10000000):
                 f.write("{}\n".format(random.random()))
 
+def getRandomFile(filename, num):
+    with open(filename, 'w') as f:
+        for i in range(int(num)):
+            f.write("{}\n".format(random.random()))
+
 if __name__ == '__main__':
     import sys
     try:
-        getRandomGFile("BigFile.txt", G=float(sys.argv[1]))
-    except:
-        print("""Usage: python GetRandomFile.py 1,
-            You need one float parameter as the size of file.""")
+        par = sys.argv[1]
+        if par.isdigit():
+            getRandomFile("BigFile.txt", int(par))
+        else:
+            getRandomGFile("BigFile.txt", G=float(sys.argv[1]))
+    except Exception as e:
+        print("error:{}".format(e))
+        print("""You need one float parameter as the G size of file or one int parameter as the line number of file.""")
